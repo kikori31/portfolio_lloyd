99cd890d157d3d505d9f26d865ca970e64f91fc8
kikori31 <kikori31@gmail.com>
2026-09-10 18:03:52 +0900

    메뉴,메인배너 작업중

---
+++ README.md
@@ -0,0 +1,3 @@
+# 로이드 리디자인 Project
+**"This is a portfolio project for educational purposes."**
+* 이것은 취업 목적의 포트폴리오 프로젝트입니다.
---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+//index.js(로이드)
+
+//item 메뉴에 올렸을 때 나오는 서브메뉴
+
+
+//메인 슬라이드 Swiper
+const introSlide = new Swiper('.main-slide',{
+    direction:'vertical',
+    mousewheel:true,
+})
