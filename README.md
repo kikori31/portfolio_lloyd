57fdfb7cc18edd539f315ca90e9662b5d1fe3067
kikori31 <kikori31@gmail.com>
2026-09-13 18:58:24 +0900

    new 섹션 작업중

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # 로이드 리디자인 Project
 **"This is a portfolio project for educational purposes."**
 * 이것은 취업 목적의 포트폴리오 프로젝트입니다.
+
+## 9.12
+* 헤더 너비 맞추기 (오른쪽 부분)
+-> nav(아이콘 3개)에 다 margin-right:24px가 들어가 있어서 헤더 너비가 맞지 않았음. 
+seach_btn과 cart_btn에 2개에만 margin-right:24px를 줘서 문제를 해결함
+* item메뉴 눌렀을 때 나오는 `목걸이 이미지` 해상도 체크하기!
+
+9.12
+* 메인 슬라이드 -> background 이미지로 변경하고 텍스트 애니메이션 주기
+* 메뉴에 올렸을때 애니메이션 주기 ->완료
+
+9/13
+* 6개 카테고리 만들기 -> 완료
+* best 섹션
+* new 섹션
--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,30 @@
 
 const itemMenu = document.querySelector('nav > a:nth-child(3)');
 const menuOpen = document.querySelector('.menu_open_wrap');
+const popup_bg = document.querySelector('.popup_bg'); 
 
 menuOpen.style.display ='none';
+popup_bg.style.display ='none';
 
 itemMenu.addEventListener('mouseover',showMenu);
 itemMenu.addEventListener('mouseout',hideMenu);
 function showMenu(){
     menuOpen.style.display = 'flex';
+    popup_bg.style.display = 'block';
 }
 function hideMenu(){
     menuOpen.style.display = 'none';
+    popup_bg.style.display = 'none';
 }
 
+//swiper 메인 배너
 
-
-//메인 슬라이드 Swiper
-// const introSlide = new Swiper('.main-slide',{
-//     direction:'vertical',
-//     mousewheel:true,
-// });
+const slide = new Swiper('.hero-banner',{
+    direction:'horizontal',
+    loop:true,
+    slidesPerView:1,
+    autoplay:{
+        delay:6000,
+        disableOnInteraction:false,
+    },
+})
