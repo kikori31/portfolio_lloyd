4c0d8d8ad6a48d5156f751d1cb964639a2339d2b
kikori31 <kikori31@gmail.com>
2026-09-14 23:40:17 +0900

    메뉴,swiper오류

--- scripts/index.js
+++ scripts/index.js
@@ -31,16 +31,132 @@ const slide = new Swiper('.hero-banner',{
         delay:6000,
         disableOnInteraction:false,
     },
-    
+    navigation:{
+    nextEl:'.swiper-button-next',
+    prevEl:'.swiper-button-prev'
+    }
 })
 
 // 베스트 카테고리 메뉴 활성화
-bestCategory.forEach((o)=>{
-    o.addEventListener('click',()=>{
-        bestCategory.forEach((o2)=>{
-            o2.classList.remove('active');
+// bestCategory.forEach((o)=>{
+//     o.addEventListener('click',()=>{
+//         bestCategory.forEach((o2)=>{
+//             o2.classList.remove('active');
+//         });
+        
+//         o.classList.add('active');
+//     });
+// });
+
+//카테고리메뉴 생성
+const categoryMenu = document.querySelector("div.category_menu > ul");
+for (let i = 0 ; i < categoryDB.length ; i++) {
+    const categoryLi = document.createElement('li');
+
+    categoryLi.innerHTML = `
+                    <a href="#">
+                        <p class="thumb"><img src="${categoryDB[i].image}" alt="${categoryDB[i].image_alt}"></p>
+                        <p class="desc">${categoryDB[i].name}</p>
+                    </a>
+    `;
+
+    categoryMenu.appendChild(categoryLi);
+}
+//3행 BEST 섹션 카테고리
+const bestCategoryGroup = document.querySelector(".best_sec > .best_category_group");
+for (let i = 0 ; i < bestCategoryDB.length ; i++) {
+    const bestCategoryBtn = document.createElement('button');
+    bestCategoryBtn.type = 'button';
+    bestCategoryBtn.className = bestCategoryDB[i].type;
+    bestCategoryBtn.textContent = bestCategoryDB[i].name;
+
+    if (i == 0) {
+        bestCategoryBtn.classList.add('active');
+    }
+
+    bestCategoryBtn.addEventListener('click',()=>{
+
+        //카테고리메뉴 버튼 전체 비활성화 처리
+        const btnGroup = document.querySelectorAll(".best_sec > .best_category_group > button");
+        btnGroup.forEach((o)=>{
+            o.classList.remove('active');
         });
         
-        o.classList.add('active');
+        //선택한 카테고리메뉴 버튼 활성화 처리
+        bestCategoryBtn.classList.add('active');
+
+        //선택한 카테고리에 맞는 상품 노출
+        showCategoryItem(bestCategoryDB[i].type);
     });
-});
+    
+    bestCategoryGroup.appendChild(bestCategoryBtn);
+}
+
+// 3행 BEST 섹션
+showCategoryItem("ring");
+
+
+//4행  NEW 섹션
+const newContents = document.querySelector(".new_sec > ul.contents");
+for(let i = 0; i < newDB.length ; i++){
+    const newLi = document.createElement('li');
+
+    newLi.innerHTML = `<a href="#">
+                            <p class="product_thumb">
+                                <img src="${newDB[i].image}" alt="${newDB[i].image_alt}">
+                            </p>
+                            <div class="product_desc">
+                                <p class="new_icon"><img src="${newDB[i].icon}" alt="${newDB[i].icon_alt}"></p>
+                                <h3 class="name">${newDB[i].name}</h3>
+                                <p class="price">${newDB[i].price.toLocaleString()}원</p>
+                                <p class="review">리뷰 ${newDB[i].review_count}</p>
+                            </div>
+                        </a>
+                    `;
+    newContents.appendChild(newLi);
+}
+
+// 7행 오늘출발 섹션
+const todayContents = document.querySelector('.today_sec > ul.contents');
+for(let i = 0; i < todayDB.length ; i++){
+    const todayLi = document.createElement('li');
+
+    todayLi.innerHTML = `<a href="#">
+                            <p class="product_thumb">
+                                <img src="${todayDB[i].image}" alt="${todayDB[i].image_alt}">
+                            </p>
+                            <div class="product_desc">
+                                <h3 class="name">${todayDB[i].name}</h3>
+                                <p class="price">${todayDB[i].price.toLocaleString()}원</p>
+                                <p class="review">리뷰 ${todayDB[i].review_count}</p>
+                            </div>
+                        </a>
+                    `;
+    todayContents.appendChild(todayLi);
+}
+
+function showCategoryItem(categoryType) {
+    const bestContents = document.querySelector('.best_sec > ul.contents');
+    bestContents.innerHTML = "";
+    
+    for(let i = 0; i < bestDB.length ; i++){
+        if (bestDB[i].type != categoryType) {
+            continue;
+        }
+
+        const bestLi = document.createElement('li');
+    
+        bestLi.innerHTML = `<a href="#">
+                                <p class="best_thumb">
+                                    <img src="${bestDB[i].image}" alt="${bestDB[i].image_alt}">
+                                </p>
+                                <div class="product_desc">
+                                    <h3 class="name">${bestDB[i].name}</h3>
+                                    <p class="price">${bestDB[i].price.toLocaleString()}원</p>
+                                    <p class="review">리뷰 ${bestDB[i].review_count}</p>
+                                </div>
+                            </a>
+                        `;
+        bestContents.appendChild(bestLi);
+    } 
+}
---
+++ scripts/lloyd_db.js
@@ -0,0 +1,521 @@
+/* lloyd_db.js */
+
+const categoryDB = [
+    {
+        id: 1,
+        name: 'GOLD',
+        image: './images/category_gold.jpg',
+        image_alt: 'gold'
+    },
+    {
+        id: 2,
+        name: '랩 다이아몬드',
+        image: './images/category_lab.jpg',
+        image_alt: '랩 다이아몬드'
+    },
+    {
+        id: 3,
+        name: '커플/웨딩',
+        image: './images/category_couple.jpg',
+        image_alt: '커플/웨딩'
+    },
+    {
+        id: 4,
+        name: '24K 순금',
+        image: './images/category_24k.jpg',
+        image_alt: '24K 순금'
+    },
+    {
+        id: 5,
+        name: '실버',
+        image: './images/category_silver.jpg',
+        image_alt: '실버'
+    },
+    {
+        id: 6,
+        name: '시계',
+        image: './images/category_watch.jpg',
+        image_alt: '시계'
+    },
+];
+
+const bestCategoryDB = [
+    {
+        id: 1,
+        type: 'ring',
+        name: '반지'
+    },
+    {
+        id: 2,
+        type: 'earring',
+        name: '귀걸이'
+    },
+    {
+        id: 3,
+        type: 'piercing',
+        name: '피어싱'
+    },
+    {
+        id: 4,
+        type: 'necklace',
+        name: '목걸이'
+    },
+    {
+        id: 5,
+        type: 'couplering',
+        name: '커플링'
+    },
+    {
+        id: 6,
+        type: 'bracelet',
+        name: '팔찌&발찌'
+    },
+    {
+        id: 7,
+        type: 'diamond',
+        name: '다이아몬드'
+    },
+    {
+        id: 8,
+        type: '24kgold',
+        name: '순금'
+    },
+    {
+        id: 9,
+        type: 'watch',
+        name: '시계'
+    },
+];
+
+const bestDB = [
+    {
+        id: 1,
+        type: 'ring',
+        name: '디어 리본 큐빅 10K 반지',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'ring',
+        name: '왕관 물고기 10K 반지',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'ring',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 반지',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'ring',
+        name: '튜더로즈가든 핑크 페어컷 14K 반지',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: 'earring',
+        name: '디어 리본 큐빅 10K 귀걸이',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'earring',
+        name: '왕관 물고기 10K 귀걸이',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'earring',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 귀걸이',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'earring',
+        name: '튜더로즈가든 핑크 페어컷 14K 귀걸이',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: 'piercing',
+        name: '디어 리본 큐빅 10K 피어싱',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'piercing',
+        name: '왕관 물고기 10K 피어싱',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'piercing',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 피어싱',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'piercing',
+        name: '튜더로즈가든 핑크 페어컷 14K 피어싱',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: 'necklace',
+        name: '디어 리본 큐빅 10K 피어싱',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'necklace',
+        name: '왕관 물고기 10K 피어싱',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'necklace',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 피어싱',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'necklace',
+        name: '튜더로즈가든 핑크 페어컷 14K 피어싱',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: 'couplering',
+        name: '디어 리본 큐빅 10K 피어싱',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'couplering',
+        name: '왕관 물고기 10K 피어싱',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'couplering',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 피어싱',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'couplering',
+        name: '튜더로즈가든 핑크 페어컷 14K 피어싱',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: 'bracelet',
+        name: '디어 리본 큐빅 10K 피어싱',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'bracelet',
+        name: '왕관 물고기 10K 피어싱',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'bracelet',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 피어싱',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'bracelet',
+        name: '튜더로즈가든 핑크 페어컷 14K 피어싱',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: 'diamond',
+        name: '디어 리본 큐빅 10K 피어싱',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'diamond',
+        name: '왕관 물고기 10K 피어싱',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'diamond',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 피어싱',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'diamond',
+        name: '튜더로즈가든 핑크 페어컷 14K 피어싱',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: '24kgold',
+        name: '디어 리본 큐빅 10K 피어싱',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: '24kgold',
+        name: '왕관 물고기 10K 피어싱',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: '24kgold',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 피어싱',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: '24kgold',
+        name: '튜더로즈가든 핑크 페어컷 14K 피어싱',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+    {
+        id: 1,
+        type: 'watch',
+        name: '디어 리본 큐빅 10K 피어싱',
+        price: 399000,
+        review_count: 50,
+        image: './images/best_thumb01.jpg',
+        image_alt: ''
+    },
+    {
+        id: 2,
+        type: 'watch',
+        name: '왕관 물고기 10K 피어싱',
+        price: 259000,
+        review_count: 20,
+        image: './images/best_thumb02.jpg',
+        image_alt: ''
+    },
+    {
+        id: 3,
+        type: 'watch',
+        name: '베이직 탄생석 12월 콜렉트링 10K 여성 피어싱',
+        price: 199000,
+        review_count: 40,
+        image: './images/best_thumb03.jpg',
+        image_alt: ''
+    },
+    {
+        id: 4,
+        type: 'watch',
+        name: '튜더로즈가든 핑크 페어컷 14K 피어싱',
+        price: 999000,
+        review_count: 29,
+        image: './images/best_thumb04.jpg',
+        image_alt: ''
+    },
+];
+
+const newDB = [
+    {
+        id: 1,
+        name: '0.095ct 6프롱 다이아몬드 14K 목걸이',
+        price: 459000,
+        review_count: 21,
+        image: './images/new_thumb01.jpg',
+        image_alt: '',
+        icon: './images/new_icon.svg',
+        icon_alt: ''
+    },
+    {
+        id: 2,
+        name: '에센셜 포인트 십자가 10K 펜던트',
+        price: 199000,
+        review_count: 12,
+        image: './images/new_thumb02.jpg',
+        image_alt: '',
+        icon: './images/new_icon.svg',
+        icon_alt: ''
+    },
+    {
+        id: 3,
+        name: '[실버 925] 빅 오벌컷 스톤 목걸이',
+        price: 99000,
+        review_count: 6,
+        image: './images/new_thumb03.jpg',
+        image_alt: '',
+        icon: './images/new_icon.svg',
+        icon_alt: ''
+    },
+    {
+        id: 4,
+        name: '포인트 세팅 14K 볼반지',
+        price: 329000,
+        review_count: 14,
+        image: './images/new_thumb04.jpg',
+        image_alt: '',
+        icon: './images/new_icon.svg',
+        icon_alt: ''
+    },
+    {
+        id: 5,
+        name: '[조카 선물 추천] 24K 순금 반돈 청룡 돌반지',
+        price: 498000,
+        review_count: 26,
+        image: './images/new_thumb05.jpg',
+        image_alt: '',
+        icon: './images/new_icon.svg',
+        icon_alt: ''
+    },
+];
+
+const todayDB = [
+    {
+        id: 1,
+        name: '하트인하트 핑크 다이아몬드 10K 목걸이',
+        price: 429000,
+        review_count: 26,
+        image: './images/today_thumb01.jpg',
+        image_alt: '',
+        icon: '',
+        icon_alt: ''
+    },
+    {
+        id: 2,
+        name: '크레센도 테니스 10K 원터치 링 귀걸이(M)',
+        price: 259000,
+        review_count: 40,
+        image: './images/today_thumb02.jpg',
+        image_alt: '',
+        icon: '',
+        icon_alt: ''
+    },
+    {
+        id: 3,
+        name: '더블 펜던트 10K 목걸이',
+        price: 429000,
+        review_count: 40,
+        image: './images/today_thumb03.jpg',
+        image_alt: '',
+        icon: '',
+        icon_alt: ''
+    },
+    {
+        id: 4,
+        name: '실버925 에센셜 파베십자가 팔찌',
+        price: 49000,
+        review_count: 20,
+        image: './images/today_thumb04.jpg',
+        image_alt: '',
+        icon: '',
+        icon_alt: ''
+    },
+    {
+        id: 5,
+        name: '쇼콜라 라운드 14K 귀걸이',
+        price: 259000,
+        review_count: 40,
+        image: './images/today_thumb05.jpg',
+        image_alt: '',
+        icon: '',
+        icon_alt: ''
+    },
+];
